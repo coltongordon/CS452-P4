fe841f79567b846810169b08c6d382f33d54aa28
Colton Gordon <coltongordon@u.boisestate.edu>
2025-04-14 15:21:13 -0600

    lab.c updated

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,12 @@
-/**Update this file with the starter code**/
+#include <lab.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h> /* for gettimeofday system call */
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <signal.h>
