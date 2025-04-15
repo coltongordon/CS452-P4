8c1d7e464b62bf48351974114067a629350c6e97
Colton Gordon <coltongordon@u.boisestate.edu>
2025-04-15 09:47:10 -0600

    Updated spacing

--- src/lab.c
+++ src/lab.c
@@ -35,7 +35,7 @@ typedef struct queue *queue_t{
     * @param capacity the maximum capacity of the queue
     * @return A fully initialized queue
     */
-   queue_t queue_init(int capacity){
+queue_t queue_init(int capacity){
     if (capacity <= 0)
         return NULL;
     queue_t q = malloc(sizeof(struct queue));
@@ -55,14 +55,15 @@ typedef struct queue *queue_t{
     pthread_cond_init(&q->not_empty, NULL);
     pthread_cond_init(&q->not_full, NULL);
     return q;
-   }
+}
+
 
 /**
     * @brief Frees all memory and related data signals all waiting threads.
     *
     * @param q a queue to free
     */
-   void queue_destroy(queue_t q){
+void queue_destroy(queue_t q){
     if (q == NULL)
         return;
     free(q->data);
@@ -72,7 +73,8 @@ typedef struct queue *queue_t{
     pthread_cond_destroy(&q->not_empty);
     pthread_cond_destroy(&q->not_full);
     return;
-   }
+}
+
 
    /**
     * @brief Adds an element to the back of the queue
@@ -80,7 +82,7 @@ typedef struct queue *queue_t{
     * @param q the queue
     * @param data the data to add
     */
-   void enqueue(queue_t q, void *data){
+void enqueue(queue_t q, void *data){
     if (q == NULL || data == NULL)
         return;
     pthread_mutex_lock(&q->lock);
@@ -97,14 +99,15 @@ typedef struct queue *queue_t{
     pthread_cond_signal(&q->not_empty);
     pthread_mutex_unlock(&q->lock);
     return;
-   }
+}
+
 
 /**
     * @brief Removes the first element in the queue.
     *
     * @param q the queue
     */
-   void *dequeue(queue_t q){
+void *dequeue(queue_t q){
     if (q == NULL)
         return NULL;
     pthread_mutex_lock(&q->lock);
@@ -121,7 +124,8 @@ typedef struct queue *queue_t{
     pthread_cond_signal(&q->not_full);
     pthread_mutex_unlock(&q->lock);
     return data;
-   }
+}
+
 
    /**
     * @brief Set the shutdown flag in the queue so all threads can
@@ -129,7 +133,7 @@ typedef struct queue *queue_t{
     *
     * @param q The queue
     */
-   void queue_shutdown(queue_t q){
+void queue_shutdown(queue_t q){
     if (q == NULL)
         return;
     pthread_mutex_lock(&q->lock);
@@ -138,32 +142,34 @@ typedef struct queue *queue_t{
     pthread_cond_broadcast(&q->not_full);
     pthread_mutex_unlock(&q->lock);
     return;
-   }
+}
+
 
     /**
     * @brief Returns true is the queue is empty
     *
     * @param q the queue
     */
-   bool is_empty(queue_t q){
+bool is_empty(queue_t q){
     if (q == NULL)
         return false;
     pthread_mutex_lock(&q->lock);
     bool empty = (q->size == 0);
     pthread_mutex_unlock(&q->lock);
     return empty;
-   }
+}
+
 
    /**
     * @brief
     *
     * @param q The queue
     */
-   bool is_shutdown(queue_t q){
+bool is_shutdown(queue_t q){
     if (q == NULL)
         return false;
     pthread_mutex_lock(&q->lock);
     bool shutdown = q->shutdown;
     pthread_mutex_unlock(&q->lock);
     return shutdown;
-   }
+}
